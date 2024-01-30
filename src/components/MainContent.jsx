1c0940ff436d2f72eb97006dbe1be687e6fb6a65
Matthew-19 <113844755+Matthew-19@users.noreply.github.com>
2024-01-30 22:14:55 +0200

    Update: More Download Options

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect, createRef } from "react";
-import { exportComponentAsJPEG } from "react-component-export-image";
+import html2canvas from "html2canvas";
 
 function MainContent() {
   const [allMemes, setAllMemes] = useState([]);
@@ -39,6 +39,49 @@ function MainContent() {
 
   const memeRef = createRef();
 
+  function handleDownload() {
+    document.getElementById("exportMemeContainer").style.display = "flex";
+
+    html2canvas(memeRef.current, {
+      logging: true,
+      useCORS: true,
+      scale: 3,
+    }).then((canvas) => {
+      let exportImage = document.createElement("a");
+      exportImage.href = canvas.toDataURL("img/png");
+      exportImage.download = "meme-download";
+      exportImage.click();
+    });
+
+    document.getElementById("exportMemeContainer").style.display = "none";
+  }
+
+  const [customDimension, setCustomDimensions] = useState({
+    width: "1920",
+    height: "1080",
+  });
+
+  function handleCustomDimensions(event) {
+    const { name, value } = event.target;
+    setCustomDimensions((prevCustomDimensions) => ({
+      ...prevCustomDimensions,
+      [name]: value,
+    }));
+  }
+
+  const [customFontSize, setCustomFontSize] = useState("16px");
+  useEffect(() => {
+    setCustomFontSize(
+      customDimension.width < 1280
+        ? "40px"
+        : customDimension.width < 1920
+        ? "50px"
+        : customDimension.width < 3840
+        ? "70px"
+        : "120px"
+    );
+  }, [customDimension.width]);
+
   return (
     <main>
       <div className="main--container">
@@ -65,24 +108,94 @@ function MainContent() {
             <button className="get-meme-btn" onClick={getMeme}>
               Get a new meme image 🖼
             </button>
-            <button
-              className="downloadBtn"
-              onClick={() =>
-                exportComponentAsJPEG(memeRef, {
-                  fileName: "Hakona Matata",
-                  html2CanvasOptions: { scale: 2 },
-                })
-              }
-            >
+            <button className="downloadBtn" onClick={handleDownload}>
               Download
             </button>
           </div>
         </div>
 
-        <div className="meme" ref={memeRef}>
+        <div className="download--group">
+          <div className="custom-dimensions">
+            <div className="custom-width-group">
+              <label htmlFor="width">Width (px)</label>
+              <input
+                type="number"
+                name="width"
+                id="width"
+                placeholder="Width"
+                value={customDimension.width}
+                onChange={handleCustomDimensions}
+                min={1}
+              />
+            </div>
+            <div className="custom-height-group">
+              <label htmlFor="height">Height (px)</label>
+              <input
+                type="number"
+                name="height"
+                id="height"
+                placeholder="Height"
+                value={customDimension.height}
+                onChange={handleCustomDimensions}
+                min={1}
+              />
+            </div>
+          </div>
+          <div className="standard-dimensions">
+            <button
+              className="720p"
+              onClick={() => setCustomDimensions({ width: 1280, height: 720 })}
+            >
+              720p
+            </button>
+            <button
+              className="1080p"
+              onClick={() => setCustomDimensions({ width: 1920, height: 1080 })}
+            >
+              1080p
+            </button>
+            <button
+              className="4k"
+              onClick={() => setCustomDimensions({ width: 3840, height: 2160 })}
+            >
+              4K
+            </button>
+          </div>
+        </div>
+
+        <div className="meme">
           <img src={meme.randomImage} alt="MemeImg" className="memeImg" />
           <p className="meme--text top">{meme.topText}</p>
           <p className="meme--text bottom">{meme.bottomText}</p>
+        </div>
+        <div
+          className="export--meme-container"
+          id="exportMemeContainer"
+          ref={memeRef}
+          style={{
+            width: `${customDimension.width}px`,
+            height: `${customDimension.height}px`,
+            display: "none",
+          }}
+        >
+          <img
+            src={meme.randomImage}
+            alt="MemeImg"
+            className="export--memeImg"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <p
+            className="meme--text top"
+            style={{ fontSize: `${customFontSize}` }}
+          >
+            {meme.topText}
+          </p>
+          <p
+            className="meme--text bottom"
+            style={{ fontSize: `${customFontSize}` }}
+          >
+            {meme.bottomText}
+          </p>
         </div>
       </div>
     </main>
